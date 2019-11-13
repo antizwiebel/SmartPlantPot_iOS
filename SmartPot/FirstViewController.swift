0bab05682cc2494eb074951b469ef8515ec44247
Mark Peneder <mpeneder@cellular.de>
2019-11-13 16:58:37 +0100

    Initial Commit

---
+++ SmartPot/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  SmartPot
+//
+//  Created by Mark on 13.11.19.
+//  Copyright © 2019 Mark. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ SmartPot/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  SmartPot
+//
+//  Created by Mark on 13.11.19.
+//  Copyright © 2019 Mark. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
